aa43084bae3bc8dcecf31e30233dd55e21f450ee
Mbn09 <ngocnguyen.150903@gmail.com>
2023-08-09 12:06:49 +0700

    Update setup

--- src/store/ref.js
+++ src/store/ref.js
@@ -1,16 +1,6 @@
-export const REGION = "asia-southeast1";
+export const REGION = "";
 // local
 export const LOCAL_USER = "Local User";
 // ref
 export const REF = {
-  // PROPERTY: "property",
-  // MEDIA: "media",
-  // CATEGORY: "category",
-  // POST: "post",
-  // TAG: "tag",
-  // CONTACT: "contact",
-  // EVENTS: "event",
-  // EVENT_TAG: "eventTag",
-  // EVENT_ENTRY: "entry",
-  // SETTINGS: "settings",
 };
